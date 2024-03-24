edaa3746d1b36b1f7c96cb387a6b67c71d6dafb2
boyin <boy_in@163.com>
2024-03-25 04:55:27 +0800

    feat: init wezterm

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,32 @@
+-- https://wezfurlong.org/wezterm/config/files.html
+local config = {}
+
+local helpers = require("helpers")
+helpers.apply_to_config(config)
+
+local wezterm = require("wezterm")
+config.font = wezterm.font("JetBrains Mono")
+config.font_size = 14.0
+
+-- timeout_milliseconds defaults to 1000 and can be omitted
+config.leader = { key = "a", mods = "CTRL", timeout_milliseconds = 1000 }
+config.keys = {
+	{
+		key = "|",
+		mods = "LEADER|SHIFT",
+		action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
+	},
+	{
+		key = "-",
+		mods = "LEADER",
+		action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }),
+	},
+	-- Send "CTRL-A" to the terminal when pressing CTRL-A, CTRL-A
+	{
+		key = "a",
+		mods = "LEADER|CTRL",
+		action = wezterm.action.SendKey({ key = "a", mods = "CTRL" }),
+	},
+}
+
+return config
