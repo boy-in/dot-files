2e14b7266011fa151bcc072fc42152d318092076
boyin <boy_in@163.com>
2024-03-25 17:21:15 +0800

    feat: add hardtime plugin

---
+++ nvim/lua/plugins/hardtime.lua
@@ -0,0 +1,12 @@
+return {
+  -- add hardtime
+  {
+    "takac/vim-hardtime",
+    -- opts = {
+    --   hardtime_default_on = 1,
+    -- },
+    -- config = function()
+    --   vim.g.hardtime_default_on = 1
+    -- end,
+  },
+}
